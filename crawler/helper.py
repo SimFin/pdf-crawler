a53eb03d029dc5914d9b25280553c4995362ad45
joseph albers <q7v6rhgfzc8tnj3d@josephs-MBP.localdomain>
2018-10-18 18:54:28 +0200

    updated crawler

---
+++ crawler/helper.py
@@ -0,0 +1,32 @@
+import logging
+from functools import lru_cache
+
+
+log = logging.getLogger(__name__)
+
+def get_content_type(response):
+	content_type = response.headers.get("Content-Type")
+	if content_type:
+		return content_type.split(';')[0]
+
+def ensure_get_response(response, session):
+	request = response.request
+	if request.method == "GET":
+		return response
+
+	response = call(session.get, request.url)
+	if not response:
+		log.debug("TODO: No GET response after HEAD. This request should"
+				  f"normally be retried or otherwise handled: {request.url}")
+
+	return response
+
+@lru_cache(maxsize=8192)
+def call(func, url):
+	try:
+		response = func(url)
+		response.raise_for_status()
+	except request.RequestException:
+		return None
+	else:
+		return response
