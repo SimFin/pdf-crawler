588c1c767cff01ad88fcc8dee55f835c0cb8f76c
Thomas Flassbeck <tf@simfin.com>
2018-10-20 11:50:26 +0200

    small corrections

--- crawler/crawl_methods.py
+++ crawler/crawl_methods.py
@@ -3,93 +3,91 @@ from selenium import webdriver
 from urllib.parse import urlparse, urljoin
 from selenium.webdriver.firefox.options import Options
 
+
 def get_hrefs_html(response, follow_foreign_hosts=False):
-	urls = []
-	soup = BeautifulSoup(response.text, "lxml")
-	parsed_response_url = urlparse(response.url)
-	urls_on_page = [link.attrs.get("href") for link in soup.find_all('a')]
+    urls = []
+    soup = BeautifulSoup(response.text, "lxml")
+    parsed_response_url = urlparse(response.url)
+    urls_on_page = [link.attrs.get("href") for link in soup.find_all('a')]
 
-	for url in urls_on_page:
-		follow = True
-		parsed_url = urlparse(url)
+    for url in urls_on_page:
+        follow = True
+        parsed_url = urlparse(url)
 
-		if not parsed_url.path:
-			continue
+        if not parsed_url.path:
+            continue
 
-		if not parsed_url.netloc:
-			url = urljoin(response.url, parsed_url.path)
-			parsed_url = urlparse(url)
+        if not parsed_url.netloc:
+            url = urljoin(response.url, parsed_url.path)
+            parsed_url = urlparse(url)
 
-		if parsed_response_url.netloc != parsed_url.netloc and not follow_foreign_hosts:
-			follow = False
+        if parsed_response_url.netloc != parsed_url.netloc and not follow_foreign_hosts:
+            follow = False
 
-		urls.append({"url":url, "follow":follow})
+        urls.append({"url": url, "follow": follow})
 
-	return urls
+    return urls
+
 
 def get_hrefs_js(response, follow_foreign_hosts=False):
-	urls = []
-	parsed_response_url = urlparse(response.url)
-	urls_on_page = response.html.absolute_links
+    urls = []
+    parsed_response_url = urlparse(response.url)
+    urls_on_page = response.html.absolute_links
 
-	for url in urls_on_page:
-		follow = True
-		parsed_url = urlparse(url)
+    for url in urls_on_page:
+        follow = True
+        parsed_url = urlparse(url)
 
-		print(str(parsed_response_url.netloc) + "\t" + parsed_url.netloc)
-		if parsed_response_url.netloc != parsed_url.netloc and not follow_foreign_hosts:
-			follow = False
+        print(str(parsed_response_url.netloc) + "\t" + parsed_url.netloc)
+        if parsed_response_url.netloc != parsed_url.netloc and not follow_foreign_hosts:
+            follow = False
 
-		urls.append({"url":url, "follow":follow})
-	
-	return urls
+        urls.append({"url": url, "follow": follow})
+
+    return urls
+
 
 def get_hrefs_js_boosted(response, follow_foreign_hosts=False):
-	urls = []
-	parsed_response_url = urlparse(response.url)
+    urls = []
+    parsed_response_url = urlparse(response.url)
 
-	# Configure driver options
-	driver_options = Options()
-	driver_options.headless = True
-	driver = webdriver.Firefox(options=driver_options)
+    # Configure driver options
+    driver_options = Options()
+    driver_options.headless = True
+    driver = webdriver.Firefox(options=driver_options)
 
-	# Open url
-	driver.get(response.url)
+    # Open url
+    driver.get(response.url)
 
-	clickable_elements = []
-	
-	elements = driver.find_elements_by_css_selector("*")
+    clickable_elements = []
 
-	# Go through all elements on page and look where the cursor is a pointer
-	for element in elements:
-		if element.value_of_css_property("cursor") == "pointer":
-			if not element.get_attribute("href"):
-				clickable_elements.append(element)
+    elements = driver.find_elements_by_css_selector("*")
 
-	# Repeat the following until there is no javascript element to click on
-	# 1. Get urls of the page
-	# 2. Click on one of the javascript elements
-	for i in range(len(clickable_elements)):
+    # Go through all elements on page and look where the cursor is a pointer
+    for element in elements:
+        if element.value_of_css_property("cursor") == "pointer":
+            if not element.get_attribute("href"):
+                clickable_elements.append(element)
 
-		urls_on_page = [link.get_attribute("href") for link in\
-						driver.find_elements_by_css_selector("a")\
-						if len(link.get_attribute("href")) > 0]
-		for url in urls_on_page:
-			follow = False
-			parsed_url = urlparse(url)
+    # Repeat the following until there is no javascript element to click on
+    # 1. Get urls of the page
+    # 2. Click on one of the javascript elements
+    for i in range(len(clickable_elements)):
 
-			if parsed_response_url.netloc != parsed_url.netloc and not follow_foreign_hosts:
-				follow = False
+        urls_on_page = [link.get_attribute("href") for link in \
+                        driver.find_elements_by_css_selector("a") \
+                        if len(link.get_attribute("href")) > 0]
+        for url in urls_on_page:
+            follow = False
+            parsed_url = urlparse(url)
 
-			urls.append({"url":url, "follow":follow})
+            if parsed_response_url.netloc != parsed_url.netloc and not follow_foreign_hosts:
+                follow = False
 
-		clickable_elements[i].click()
-	
-	driver.close()
+            urls.append({"url": url, "follow": follow})
 
-	return urls
+        clickable_elements[i].click()
 
+    driver.close()
 
-
-
-
+    return urls
--- crawler/crawler.py
+++ crawler/crawler.py
@@ -4,60 +4,60 @@ from crawler.crawl_methods import get_hrefs_html, get_hrefs_js, get_hrefs_js_boo
 
 
 class Crawler:
+    def __init__(self, downloader, get_handlers=None, head_handlers=None, follow_foreign_hosts = False):
 
-	def __init__(self, downloader, get_handlers=None, head_handlers=None):
+        # Crawler internals
+        self.downloader = downloader
+        self.get_handlers = get_handlers or {}
+        self.head_handlers = head_handlers or {}
+        self.session = self.downloader.session()
 
-		# Crawler internals
-		self.downloader = downloader
-		self.get_handlers = get_handlers or {}
-		self.head_handlers = head_handlers or {}
-		session = self.downloader.session()
+        # Crawler information
+        self.get_handled = set()
+        self.head_handled = set()
+        self.follow_foreign = follow_foreign_hosts
 
-		# Crawler information
-		self.get_handled = set()
-		self.head_handled = set()
+    def crawl(self, url, depth, previous_url=None, follow=True):
+        response = call(self.session.head, url) or call(self.session.get, url)
+        if not response:
+            return
 
-	def crawl(self, url, depth, previous_url=None, follow=True):
-		response = call(self.session.head, url) or call(self.session.get, url)
-		if not response:
-			return
+        # Type of content on page at url
+        content_type = get_content_type(response)
 
-		# Type of content on page at url
-		content_type = get_content_type(response)
+        # Name of pdf
+        local_name = None
 
-		# Name of pdf
-		local_name = None
+        get_handler = self.get_handlers.get(content_type)
+        if get_handler and url not in self.get_handled:
+            response = ensure_get_response(response, self.session)
+            if response:
+                local_name = get_handler.handle(response)
+                self.get_handled.add(url)
 
-		get_handler = self.get_handlers.get(content_type)
-		if get_handler and url not in self.get_handled:
-			response = ensure_get_response(response, self.session)
-			if response:
-				local_name = get_handler.handle(response)
-				self.get_handled.add(url)
+        head_handler = self.head_handlers.get(content_type)
+        if head_handler and url not in self.head_handled:
+            head_handler.handle(response, depth, previous_url, local_name)
+            self.head_handled.add(url)
 
-		head_handler = self.head_handlers.get(content_type)
-		if head_handler and url not in self.head_handled:
-			head_handler.handle(response, depth, previous_url, local_name)
-			self.head_handled.add(url)
+        if content_type == "text/html" and depth and follow:
+            response = ensure_get_response(response, self.session)
+            if not response:
+                return
+            depth -= 1
+            urls = self.get_urls(response, method="html")
+            for next_url in tqdm(urls):
+                self.crawl(next_url['url'], depth, previous_url=url, follow=next_url['follow'])
 
-		if content_type == "text/html" and depth and follow:
-			response = _ensure_get_response(response, session)
-			if not response:
-				return
-			depth -= 1
-			urls = self.get_urls(response, stage="javascript")
-			for next_url in tqdm(urls):
-				self.crawl(next_url['url'], depth, previous_url=url,follow=next_url['follow'])
+    def get_urls(self, response, method="html"):
+        if method == "html":
+            urls = get_hrefs_html(response,self.follow_foreign)
+        elif method == "javascript":
+            urls = get_hrefs_js(response,self.follow_foreign)
+        elif method == "javascript_boosted":
+            urls = get_hrefs_js_boosted(response,self.follow_foreign)
+        else:
+            print("No valid scrape method.")
+            return
 
-	def get_urls(self, response, stage="html"):
-		if stage == "html":
-			urls = get_hrefs_html(response)
-		elif stage == "javascript":
-			urls = get_hrefs_js(response)
-		elif stage == "javascript_boosted":
-			urls = get_hrefs_js_boosted(response)
-		else:
-			print("No valid scrape method.")
-			return
-
-		return urls
+        return urls
